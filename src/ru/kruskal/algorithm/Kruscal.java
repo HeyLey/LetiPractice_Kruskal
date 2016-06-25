d4088eae85deb8aeff9641177c26b43b4110bb3f
anara <anara.75197@gmail.com>
2016-06-26 03:10:15 +0600

    add algo template

--- src/ru/kruskal/algorithm/Kruscal.java
+++ src/ru/kruskal/algorithm/Kruscal.java
@@ -1,7 +1,53 @@
 package ru.kruskal.algorithm;
 
+import ru.kruskal.model.Edge;
+import ru.kruskal.model.Graph;
+import java.util.List;
+
 /**
  *
  */
 public class Kruscal {
+    int[] set; //номер множества
+    int[] rnk; //ранг
+    List<Edge> mst;
+    Graph gr; //исходный граф
+
+    Kruscal(int size){
+        set = new int [size];
+        rnk = new int [size];
+        for (int i = 0; i<size; i++){
+            set[i]=i;
+        }
+    }
+    /* Возвращает множество, которому принадлежит x */
+    int set(int x){
+        return x == set[x] ? x : (set[x] = set(set[x]));
+    }
+
+    boolean union(int u, int v){
+        if ( (u=set(u)) == (v=set(v)) )
+            return false;
+        if (rnk[u] < rnk[v])
+            set[u] = v;
+        else{
+            set[v]=u;
+            if (rnk[u]==rnk[v])
+                rnk[u]++;
+        }
+        return true;
+    }
+    public void kr(){
+        gr.sortGr(); //сортируем ребра
+
+        int sum =0;
+        for (Edge e : edges){
+            if (this.union(e.v1, e.v2))
+                sum+=e.weight;
+        }
+
+
+    }
+
+
 }
