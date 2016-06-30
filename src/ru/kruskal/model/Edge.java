6c4031c683e23f435fb4acba95ded6c34da0f916
HeyLey <leila.xr@gmail.com>
2016-06-30 13:45:17 +0200

    Edge implements Comparable

--- src/ru/kruskal/model/Edge.java
+++ src/ru/kruskal/model/Edge.java
@@ -3,7 +3,7 @@ package ru.kruskal.model;
 /**
  * @author LeylaH
  */
-public class Edge {
+public class Edge implements Comparable<Edge> {
     Vertex v1;
     Vertex v2;
     int weight;
@@ -12,5 +12,22 @@ public class Edge {
         this.v1 = v1;
         this.v2 = v2;
         this.weight = weight;
+    }
+
+    /**
+     * Compares this object with the specified object for order.  Returns a
+     * negative integer, zero, or a positive integer as this object is less
+     * than, equal to, or greater than the specified object.
+     * <p>
+     * @param o the object to be compared.
+     * @return a negative integer, zero, or a positive integer as this object
+     * is less than, equal to, or greater than the specified object.
+     * @throws NullPointerException if the specified object is null
+     * @throws ClassCastException   if the specified object's type prevents it
+     *                              from being compared to this object.
+     */
+    @Override
+    public int compareTo(Edge o) {
+        return weight > o.weight ? 1 : weight == o.weight ? 0 : -1;
     }
 }
