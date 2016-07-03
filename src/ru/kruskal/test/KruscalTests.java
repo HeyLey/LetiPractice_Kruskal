d40a09f091a3310c71041adebe822b224c872faf
HeyLey <leila.xr@gmail.com>
2016-07-04 01:06:26 +0200

    add junit tests package

---
+++ src/ru/kruskal/test/KruscalTests.java
@@ -0,0 +1,13 @@
+package ru.kruskal.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class KruscalTests {
+
+
+}
