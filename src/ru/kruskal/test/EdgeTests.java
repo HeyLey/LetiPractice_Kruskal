47b2578c6869e9a44ffd9909f0b024e2d1aebf59
HeyLey <leila.xr@gmail.com>
2016-07-04 02:15:25 +0200

    add JUnit tests templates

---
+++ src/ru/kruskal/test/EdgeTests.java
@@ -0,0 +1,12 @@
+package ru.kruskal.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class EdgeTests {
+
+    @Test
+    public void testCompareTo() {
+
+    }
+}
---
+++ src/ru/kruskal/test/GraphTests.java
@@ -0,0 +1,18 @@
+package ru.kruskal.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class GraphTests {
+
+    @Test
+    public void testsSorting(){
+
+    }
+
+    @Test
+    public void testAddEdge() {
+
+
+    }
+}
